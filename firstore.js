30b6b3b2efbe6fe868061a4743ac25cffd53c467
TanteJossa <63878555+TanteJossa@users.noreply.github.com>
2026-01-05 14:49:06 +0100

    Remove Firebase configuration from firstore.js
    Removed Firebase configuration details from the file.

--- firstore.js
+++ firstore.js
@@ -1,10 +1,4 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIxeHOP2T39mp83RKPO_bmoBvsqCUhmtk",
-  authDomain: "joost-koch.firebaseapp.com",
-  projectId: "joost-koch",
-  storageBucket: "joost-koch.firebasestorage.app",
-  messagingSenderId: "234817865209",
-  appId: "1:234817865209:web:ef865f6b8888a45b202928",
-  measurementId: "G-NWWE51QX8Y"
+  
 };
